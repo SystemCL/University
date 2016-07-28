529563882355c38498caf47336bb14698f5ccc3c
vploaia <vploaia@P5MD-PC070.pentalog.com>
2016-07-28 11:01:42 +0300

    block try/catch in StoreData

--- src/test/java/testwithrelationships/StoreData.java
+++ src/test/java/testwithrelationships/StoreData.java
@@ -12,19 +12,28 @@ public class StoreData {
 	public static void main(String[] args) {
 		Configuration cfg = new Configuration();
 		Session session = cfg.configure("hibernate.cfg.xml").buildSessionFactory().openSession();
+		Transaction t = null;
+		try {
+			t = session.beginTransaction();
+			Student s1 = new Student();
+			s1.setId(11);
+			s1.setFirstName("QWERTY");
+			s1.setLastName("CCCCCCC");
+			s1.setAge(21);
+			s1.setEnrolment_date(null);
+			s1.setGroup(null);
+			
+			session.persist(s1);
+			t.commit();
+			
+			
+		} catch (Exception e) {
+			if(t!=null) t.rollback();
+			e.printStackTrace();
+		} finally{
+			session.close();
+		}	
 		
-		Transaction t = session.beginTransaction();
-		
-		Student s1 = new Student();
-		s1.setId(10);
-		s1.setFirstName("QWERTY");
-		s1.setLastName("CCCCCCC");
-		s1.setAge(21);
-		s1.setEnrolment_date(null);
-		s1.setGroup(null);
-		
-		session.persist(s1);
-		t.commit();
 		session.close();
 		System.out.println("Success insert!");
 	}
